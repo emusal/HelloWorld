063ccbbf386eb1d019971d785164aed7d9297564
Moohyoung KIM <emusal@gmail.com>
2021-08-02 11:42:18 +0000

    Remove the code that delete container after run.sh executed.

--- flask/run.sh
+++ flask/run.sh
@@ -1,6 +1,6 @@
 docker build . -t github.io/emusal/helloworld/flask
 docker run -p 49160:8080 -d --name helloworld_flask github.io/emusal/helloworld/flask 
-sleep 3
-curl -i http://localhost:49160
-docker stop helloworld_flask
-docker rm helloworld_flask
+#sleep 3
+#curl -i http://localhost:49160
+#docker stop helloworld_flask
+#docker rm helloworld_flask
--- javascript/run.sh
+++ javascript/run.sh
@@ -1,6 +1,6 @@
 docker build . -t github.io/emusal/helloworld/javascript
 docker run -p 49160:8080 -d --name helloworld_javascript github.io/emusal/helloworld/javascript 
-sleep 3
-curl -i http://localhost:49160
-docker stop helloworld_javascript
-docker rm helloworld_javascript
+#sleep 3
+#curl -i http://localhost:49160
+#docker stop helloworld_javascript
+#docker rm helloworld_javascript
